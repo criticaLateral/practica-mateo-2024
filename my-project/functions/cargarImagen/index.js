d1e92021d8dc98978bac016edf94136891ac48e1
montoyamoraga <montoyamoraga@gmail.com>
2024-08-19 16:47:12 -0400

    arreglar brackets para no tener errores consola

--- my-project/functions/cargarImagen/index.js
+++ my-project/functions/cargarImagen/index.js
@@ -1,5 +1,5 @@
 export const handler = ({ inputs, mechanic, sketch }) => {
-  const { width, height, image, color, nivelThreshold, halftoneEnabled, halftoneImg, halftoneImage } = inputs;
+  const { ancho, height, image, color, nivelThreshold, halftoneEnabled, halftoneImg, halftoneImage } = inputs;
 
   let img;
   let imgGraphic;
@@ -13,7 +13,6 @@ export const handler = ({ inputs, mechanic, sketch }) => {
     imgGraphic.fill(color);
     imgGraphic.rect(0, 0, img.width, img.height);
     imgGraphic.blendMode(imgGraphic.BLEND);
-    
   };
 
   const putImageOnCanvas = () => {
@@ -21,30 +20,30 @@ export const handler = ({ inputs, mechanic, sketch }) => {
     // calcular la relación de aspecto de la imagen
     const imageAspectRatio = imgGraphic.width / imgGraphic.height;
 
-    // dimensiones máximas del canvas 
+    // dimensiones maximas del canvas 
     const maxWidth = window.innerWidth;
     const maxHeight = window.innerHeight;
 
     // calcular las nuevas dimensiones del canvas
     let newWidth, newHeight;
     if (imageAspectRatio > maxWidth / maxHeight) {
-      // si la imagen es más ancha
+      // si la imagen es mas ancha
       newWidth = maxWidth;
       newHeight = maxWidth / imageAspectRatio;
     } else {
-      // si la imagen es más alta
+      // si la imagen es mas alta
       newHeight = maxHeight;
       newWidth = maxHeight * imageAspectRatio;
     }
 
-    // ajustar el tamaño del canvas
+    // ajustar el tamano del canvas
     sketch.resizeCanvas(newWidth, newHeight);
 
-    // calcular el tamaño escalado de la imagen para que quepa en el canvas
+    // calcular el tamano escalado de la imagen para que quepa en el canvas
     let scaledWidth = newWidth;
     let scaledHeight = newHeight;
 
-    // ajustar el tamano para imágenes cuadradas y evitar cortes
+    // ajustar el tamano para imagenes cuadradas y evitar cortes
     if (imgGraphic.width === imgGraphic.height) {
     // si la imagen es cuadrada, ajustamos al tamano más pequeño del canvas
       scaledWidth = Math.min(newWidth, newHeight);
@@ -65,7 +64,6 @@ export const handler = ({ inputs, mechanic, sketch }) => {
 
     // dibujar la imagen en el canvas
     sketch.image(imgGraphic, x, y, scaledWidth, scaledHeight);
-
   };
 
   const setStylingBase = () => {
@@ -77,24 +75,13 @@ export const handler = ({ inputs, mechanic, sketch }) => {
   sketch.preload = () => {
     if (image) {
       img = sketch.loadImage(URL.createObjectURL(image));
-      if (halftoneEnabled) {
-        halftoneImg = halftoneImage(imgGraphic, {
-          angle: 45,
-          dotSize: 5,
-          pattern: "circles",
-          noise: 0.1,
-          color: "black",
-          opacity: 0.8
-        });
-      }
     } else {
       img = sketch.loadImage("static/imagenDePrueba.png");
     }
   };
 
   sketch.setup = () => {
-
-    sketch.createCanvas(width, height);
+    sketch.createCanvas(ancho, height);
     if (img) {
       loadImageAndAddFilter();
     }
@@ -107,23 +94,10 @@ export const handler = ({ inputs, mechanic, sketch }) => {
       putImageOnCanvas();
     };
 
-    if (img) {
-      if (halftoneEnabled) {
-        sketch.image(halftoneImg, x, y, scaledWidth, scaledHeight);
-      } else {
-        sketch.image(img, x, y, scaledWidth, scaledHeight);
-      };
-    };
-
-    sketch.loadImage();
-
-    sketch.image(imgGraphic, x, y, scaledWidth, scaledHeight);
-
-    drawRiso();
-
+    mechanic.done();
   };
 
-    mechanic.done();
+   
 };
 
 
@@ -131,7 +105,7 @@ export const inputs = {
   image: {
     type: "image",
   },
-  width: {
+  ancho: {
     type: "number",
     default: 500,
     editable: true
@@ -163,20 +137,19 @@ export const inputs = {
 
 export const presets = {
   vertical: {
-    width: 1080,
+    ancho: 1080,
     height: 1920  
   },
   horizontal: {
-    width: 1920,
+    ancho: 1920,
     height: 1080
   },
   cuadrado: {
-    width: 1920,
+    ancho: 1920,
     height: 1920
   },
 };
 
 export const settings = {
-  engine: require("@mechanic-design/engine-p5")
-  
+  engine: require("@mechanic-design/engine-p5"),
 };

48d0d786a5b333f94bcea59c8fb9f6b452f92ba8
montoyamoraga <montoyamoraga@gmail.com>
2024-08-19 15:56:51 -0400

    cambiar comentarios a inicio con minuscula y quitar caracteres espanol

--- my-project/functions/cargarImagen/index.js
+++ my-project/functions/cargarImagen/index.js
@@ -1,7 +1,4 @@
-import { Mechanic } from "@mechanic-design/core";
 // import { halftoneImage } from 'p5';
-import p5 from 'p5';
-
 
 // import {
 // halftoneEnabled,
@@ -15,8 +12,6 @@ export const handler = ({ inputs, mechanic, sketch }) => {
   const { width, height, image, color, nivelThreshold, halftoneEnabled, halftoneImg, halftoneImage } = inputs;
 
   const rows = 32;
-  const separation = height / rows;
-  const availableRows = Array.from({ length: rows }, (_, k) => k);
 
   let img;
   let imgGraphic;
@@ -35,39 +30,39 @@ export const handler = ({ inputs, mechanic, sketch }) => {
 
   const putImageOnCanvas = () => {
   
-    // Calcular la relación de aspecto de la imagen
+    // calcular la relación de aspecto de la imagen
     const imageAspectRatio = imgGraphic.width / imgGraphic.height;
 
-    // Dimensiones máximas del canvas 
+    // dimensiones máximas del canvas 
     const maxWidth = window.innerWidth;
     const maxHeight = window.innerHeight;
 
-    // Calcular las nuevas dimensiones del canvas
+    // calcular las nuevas dimensiones del canvas
     let newWidth, newHeight;
     if (imageAspectRatio > maxWidth / maxHeight) {
-      // La imagen es más ancha
+      // si la imagen es más ancha
       newWidth = maxWidth;
       newHeight = maxWidth / imageAspectRatio;
     } else {
-      // La imagen es más alta
+      // si la imagen es más alta
       newHeight = maxHeight;
       newWidth = maxHeight * imageAspectRatio;
     }
 
-    // Ajustar el tamaño del canvas
+    // ajustar el tamaño del canvas
     sketch.resizeCanvas(newWidth, newHeight);
 
-    // Calcular el tamaño escalado de la imagen para que quepa en el canvas
+    // calcular el tamaño escalado de la imagen para que quepa en el canvas
     let scaledWidth = newWidth;
     let scaledHeight = newHeight;
 
-    // Ajustar el tamaño para imágenes cuadradas y evitar cortes
+    // ajustar el tamano para imágenes cuadradas y evitar cortes
     if (imgGraphic.width === imgGraphic.height) {
-    // Si la imagen es cuadrada, ajustamos al tamaño más pequeño del canvas
+    // si la imagen es cuadrada, ajustamos al tamano más pequeño del canvas
       scaledWidth = Math.min(newWidth, newHeight);
       scaledHeight = scaledWidth;
     } else {
-    // Para imágenes no cuadradas, evitamos que se corte
+    // para imagenes no cuadradas, evitamos que se corte
       if (imgGraphic.width > newWidth) {
         scaledWidth = imgGraphic.width;
       }
@@ -76,11 +71,11 @@ export const handler = ({ inputs, mechanic, sketch }) => {
       }
     }
 
-    // Centrar la imagen en el canvas
+    // centrar la imagen en el canvas
     const x = (newWidth - scaledWidth) / 2;
     const y = (newHeight - scaledHeight) / 2;
 
-    // Dibujar la imagen en el canvas
+    // dibujar la imagen en el canvas
     sketch.image(imgGraphic, x, y, scaledWidth, scaledHeight);
 
   };
@@ -187,15 +182,15 @@ export const inputs = {
 };
 
 export const presets = {
-  Vertical: {
+  vertical: {
     width: 1080,
     height: 1920  
   },
-  Horizontal: {
+  horizontal: {
     width: 1920,
     height: 1080
   },
-  Cuadrado: {
+  cuadrado: {
     width: 1920,
     height: 1920
   },

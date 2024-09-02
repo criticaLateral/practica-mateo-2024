34368f6d358f1089af30e6343f7b6402524e0294
matbutom <163034603+matbutom@users.noreply.github.com>
2024-09-02 15:44:07 -0400

    ascci y orden general

--- my-project/functions/glitch/index.js
+++ my-project/functions/glitch/index.js
@@ -10,39 +10,29 @@ export const handler = ({ inputs, mechanic, sketch }) => {
   };
 
   const putImageOnCanvas = () => {
-    // Calcular la relación de aspecto de la imagen
     const imageAspectRatio = imgGraphic.width / imgGraphic.height;
 
-    // Dimensiones máximas del canvas
     const maxWidth = window.innerWidth;
     const maxHeight = window.innerHeight;
 
-    // Calcular las nuevas dimensiones del canvas
     let newWidth, newHeight;
     if (imageAspectRatio > maxWidth / maxHeight) {
-      // La imagen es más ancha
       newWidth = maxWidth;
       newHeight = maxWidth / imageAspectRatio;
     } else {
-      // La imagen es más alta
       newHeight = maxHeight;
       newWidth = maxHeight * imageAspectRatio;
     }
 
-    // Ajustar el tamaño del canvas
     sketch.resizeCanvas(newWidth, newHeight);
 
-    // Calcular el tamaño escalado de la imagen para que quepa en el canvas
     let scaledWidth = newWidth;
     let scaledHeight = newHeight;
 
-    // Ajustar el tamaño para imágenes cuadradas y evitar cortes
     if (imgGraphic.width === imgGraphic.height) {
-      // Si la imagen es cuadrada, ajustamos al tamaño más pequeño del canvas
       scaledWidth = Math.min(newWidth, newHeight);
       scaledHeight = scaledWidth;
     } else {
-      // Para imágenes no cuadradas, evitamos que se corte
       if (imgGraphic.width > newWidth) {
         scaledWidth = imgGraphic.width;
       }
@@ -50,12 +40,9 @@ export const handler = ({ inputs, mechanic, sketch }) => {
         scaledHeight = imgGraphic.height;
       }
     }
-
-    // Centrar la imagen en el canvas
     const x = (newWidth - scaledWidth) / 2;
     const y = (newHeight - scaledHeight) / 2;
 
-    // dibujar la imagen en el canvas
     sketch.image(imgGraphic, x, y, scaledWidth, scaledHeight);
   };
 

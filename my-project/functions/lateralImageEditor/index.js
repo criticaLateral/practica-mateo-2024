21205fff2ace4c0156d73211366b3fcf44efb793
matbutom <163034603+matbutom@users.noreply.github.com>
2024-11-15 13:29:56 -0300

    comentarios completos a lateralImageEditor

--- my-project/functions/lateralImageEditor/index.js
+++ my-project/functions/lateralImageEditor/index.js
@@ -5,6 +5,7 @@
 // manual de uso
 
 // parámetros que vamos a usar
+
 // cargar imagen
 
 // colores
@@ -22,7 +23,6 @@
 
 // efecto2: bitmap
 // columnas para el efecto (densidad)
-// uso de círculos y cuadrados
 
 // efecto3 pixelado
 // columnas para el efecto (densidad)
@@ -50,17 +50,14 @@ export const handler = ({ inputs, mechanic, sketch }) => {
   // variables de los efectos aplicados a las imagenes
   let imgOriginal;
   let imgGraphic;
-  let imgPixelada;
-  let imgThreshold;
 
   // funcion para cargar imagen y filtros
   const cargarImagenYFiltro = () => {
-
     // crear un imgGraphic tan grande como el original
     imgGraphic = sketch.createGraphics(imgOriginal.width, imgOriginal.height);
     imgGraphic.image(imgOriginal, 0, 0);
 
-  // efecto0: blend
+    // efecto0: blend
 
     // variable para aplicar el efecto de blend colors
     if (habilitarBlend) {
@@ -81,51 +78,64 @@ export const handler = ({ inputs, mechanic, sketch }) => {
 
     // efecto1: threshold
 
-
     // variable para efecto threshold
     if (habilitarThreshold) {
+      // aplica el filtro de umbral a la imagen
+      // el valor "nivelUmbral" determina el nivel de umbral con un slider
       imgGraphic.filter(imgGraphic.THRESHOLD, nivelThreshold);
-
       imgGraphic.loadPixels();
 
+      // obtiene los valores de rojo, verde y azul del color primario
       let rojoPrimario = imgGraphic.red(colorPrimario);
       let verdePrimario = imgGraphic.green(colorPrimario);
       let azulPrimario = imgGraphic.blue(colorPrimario);
 
+      // itera a través de cada píxel de la imagen (saltando el canal alfa)
       for (let i = 0; i < imgGraphic.pixels.length; i = i + 4) {
+        let brillo =
+          (imgGraphic.pixels[i + 0] +
+            imgGraphic.pixels[i + 1] +
+            imgGraphic.pixels[i + 2]) /
+          3;
 
-       let brillo = (imgGraphic.pixels[i + 0] + imgGraphic.pixels[i + 1] + imgGraphic.pixels[i + 2]) / 3;
+        // aplica el efecto de umbral a cada canal de color basado en el brillo
+        // multiplica el brillo por el valor del color primario para teñir la imagen
 
         // rojo
-        imgGraphic.pixels[i + 0] = (brillo / 255) *  rojoPrimario;
+        imgGraphic.pixels[i + 0] = (brillo / 255) * rojoPrimario;
         // azul
         imgGraphic.pixels[i + 1] = (brillo / 255) * verdePrimario;
         // verde
         imgGraphic.pixels[i + 2] = (brillo / 255) * azulPrimario;
-        // transparencia
+        // establece el canal alfa (transparencia) a completamente opaco (255)
         imgGraphic.pixels[i + 3] = 255;
       }
 
       imgGraphic.updatePixels();
     }
 
-
-        // efecto2: bitmap
-
+    // efecto2: bitmap
 
     if (habilitarBitmap) {
       // cálculos base adaptados de
       // https://tabreturn.github.io/code/processing/python/2019/02/09/processing.py_in_ten_lessons-6.3-_halftones.html
+
+      // numero total de columnas en el mapa de bits
       let colTotal = columnasBitmap;
+      // Tamaño de cada celda en el mapa de bits (basado en el ancho de la imagen original)
       let cellSize = imgOriginal.width / colTotal;
+      // numero total de filas en el mapa de bits (redondeado basado en el tamaño de celda)
       let rowTotal = Math.round(imgOriginal.height / cellSize);
+      // indice de columna y fila actuales (para iterar a través de las celdas)
       let col = 0;
       let row = 0;
+      // bucle que recorre cada celda en la cuadrícula del mapa de bits (total de columnas * filas)
       for (let i = 0; i < colTotal * rowTotal; i++) {
         let x = col * cellSize;
         let y = row * cellSize;
+        // pasamos a la siguiente columna después de calcular las coordenadas
         col = col + 1;
-
+        // verificamos si necesitamos pasar a la siguiente fila
         if (col >= colTotal) {
           col = 0;
           row = row + 1;
@@ -133,94 +143,101 @@ export const handler = ({ inputs, mechanic, sketch }) => {
         x = x + cellSize / 2;
         y = y + cellSize / 2;
         let colorPixel = imgGraphic.get(x, y);
-        // variable para aplicar efecto de circulos o cuadrados
-        // NEXT: proxima version, podemos limpiar la logica de estos booleans
 
-          imgGraphic.noStroke();
-          imgGraphic.fill(colorPixel);
-          imgGraphic.rect(
-            x - cellSize / 2,
-            y - cellSize / 2,
-            cellSize,
-            cellSize
-          );
-        
+        imgGraphic.noStroke();
+        imgGraphic.fill(colorPixel);
+        imgGraphic.rect(x - cellSize / 2, y - cellSize / 2, cellSize, cellSize);
       }
     }
 
     // efecto3 pixelado
 
     // variable para efecto de pixelado threshold
-    let umbralPixelado = 255.0/2.0;
+    let umbralPixelado = 255.0 / 2.0;
 
     if (habilitarPixelado) {
-
+      // obtiene los valores rojo, verde y azul del color primario
       let primarioR = imgGraphic.red(colorPrimario);
       let primarioG = imgGraphic.green(colorPrimario);
       let primarioB = imgGraphic.blue(colorPrimario);
 
+      // obtiene los valores rojo, verde y azul del color secundario
       let secundarioR = imgGraphic.red(colorSecundario);
       let secundarioG = imgGraphic.green(colorSecundario);
       let secundarioB = imgGraphic.blue(colorSecundario);
 
       imgGraphic.filter(imgGraphic.GRAY);
 
+      // numero total de columnas en la cuadrícula pixelada
       let colTotal = imgOriginal.width / resolucionPixelado;
       let rowTotal = Math.round(imgOriginal.height / resolucionPixelado);
       let col = 0;
       let row = 0;
+
+      // bucle que recorre cada cuadro en la cuadrícula pixelada (total de columnas * filas)
       for (let i = 0; i < colTotal * rowTotal; i++) {
+        // calcula las coordenadas x e y del centro del cuadro actual
         let x = col * resolucionPixelado;
         let y = row * resolucionPixelado;
+        // pasamos a la siguiente columna después de calcular las coordenadas
         col = col + 1;
 
+        // verificamos si necesitamos pasar a la siguiente fila
         if (col >= colTotal) {
           col = 0;
           row = row + 1;
         }
         x = x + resolucionPixelado / 2;
         y = y + resolucionPixelado / 2;
+        // obtenemos el valor de color del píxel en las coordenadas calculadas
         let colorPixel = imgGraphic.get(x, y);
-
-        let brillo = (imgGraphic.red(colorPixel) + imgGraphic.green(colorPixel) + imgGraphic.blue(colorPixel)) / 3.0;
+        // calcula el brillo promedio del píxel (rojo, verde y azul)
+        let brillo =
+          (imgGraphic.red(colorPixel) +
+            imgGraphic.green(colorPixel) +
+            imgGraphic.blue(colorPixel)) /
+          3.0;
 
         imgGraphic.noStroke();
-
+        // elige el color de relleno según el brillo
         if (brillo < umbralPixelado) {
+          // si el brillo es menor al umbral, se usa el color secundario
           imgGraphic.fill(secundarioR, secundarioG, secundarioB);
-        }
-        else {
+        } else {
+          // si el brillo es mayor o igual al umbral, se usa el color primario
           imgGraphic.fill(primarioR, primarioG, primarioB);
         }
-          imgGraphic.rect(
-            x - resolucionPixelado / 2,
-            y - resolucionPixelado / 2,
-            resolucionPixelado,
-            resolucionPixelado
-          );
-        
+        imgGraphic.rect(
+          x - resolucionPixelado / 2,
+          y - resolucionPixelado / 2,
+          resolucionPixelado,
+          resolucionPixelado
+        );
       }
     }
   };
 
   // calculos para cargar imagen en el canvas
   const ponerImagenEnCanvas = () => {
+    // calculamos la relacion de aspecto de la imagen
     const imageAspectRatio = imgGraphic.width / imgGraphic.height;
-
+    // obtenemos el ancho y alto maximo de la ventana
     const maxWidth = window.innerWidth;
     const maxHeight = window.innerHeight;
-
+    // calculamos el nuevo ancho y alto para que la imagen quepa en la ventana
     let newWidth, newHeight;
     if (imageAspectRatio > maxWidth / maxHeight) {
+      // si la imagen es mas ancha que alta, ajustamos el ancho al maximo y calculamos el alto
       newWidth = maxWidth;
       newHeight = maxWidth / imageAspectRatio;
     } else {
+      // si la imagen es mas alta que ancha, ajustamos el alto al maximo y calculamos el ancho
       newHeight = maxHeight;
       newWidth = maxHeight * imageAspectRatio;
     }
-
+    // redimensionamos el canvas al nuevo ancho y alto
     sketch.resizeCanvas(newWidth, newHeight);
-
+    // calculamos el ancho y alto escalados para la imagen
     let scaledWidth = newWidth;
     let scaledHeight = newHeight;
 
@@ -228,9 +245,11 @@ export const handler = ({ inputs, mechanic, sketch }) => {
     // canvas responsivo a imagen
 
     if (imgGraphic.width === imgGraphic.height) {
+      // si la imagen es cuadrada, usamos el menor de los dos valores para el ancho y alto escalados
       scaledWidth = Math.min(newWidth, newHeight);
       scaledHeight = scaledWidth;
     } else {
+      // si la imagen no es cuadrada, escalamos solo si es necesario para que quepa en el canvas
       if (imgGraphic.width > newWidth) {
         scaledWidth = imgGraphic.width;
       }
@@ -238,29 +257,30 @@ export const handler = ({ inputs, mechanic, sketch }) => {
         scaledHeight = imgGraphic.height;
       }
     }
+    // calculamos la posicion x e y para centrar la imagen en el canvas
     const x = (newWidth - scaledWidth) / 2;
     const y = (newHeight - scaledHeight) / 2;
 
     // renderizamos imagen
-
     sketch.image(imgGraphic, x, y, scaledWidth, scaledHeight);
-
   };
 
-  // explicar que estas son las condiciones de inicio
+  // definimos el estilo base del canvas
   const definirEstiloBase = () => {
-    // fondo blanco
+    // establecemos el fondo blanco
     sketch.background("white");
-    // borde con el color primario
+    // establecemos el color del borde al color primario
     sketch.stroke(colorPrimario);
-    // relleno con el color primario
+    // establecemos el color de relleno al color primario
     sketch.fill(colorPrimario);
   };
-
+  // cargamos la imagen antes de que el sketch comience
   sketch.preload = () => {
     if (imagen) {
+      // si hay una imagen seleccionada, la cargamos desde la url
       imgOriginal = sketch.loadImage(URL.createObjectURL(imagen));
     } else {
+      // si no hay imagen seleccionada, cargamos una imagen por defecto
       imgOriginal = sketch.loadImage("static/featured.jpg");
     }
   };
@@ -283,22 +303,23 @@ export const handler = ({ inputs, mechanic, sketch }) => {
   };
 };
 
+// inputs reflejados en la barra lateral
 export const inputs = {
   imagen: {
     type: "image",
-    label: "GENERAL - cargar imagen"
+    label: "GENERAL - cargar imagen",
   },
   colorPrimario: {
     type: "color",
     default: "#39ff14",
     model: "hex",
-    label: "GENERAL - color primario"
+    label: "GENERAL - color primario",
   },
   colorSecundario: {
     type: "color",
     default: "#DE3163",
     model: "hex",
-    label: "GENERAL - color secundario"
+    label: "GENERAL - color secundario",
   },
   habilitarBlend: {
     type: "boolean",
@@ -334,7 +355,7 @@ export const inputs = {
     step: 1.0,
     slider: true,
     default: 100.0,
-    label: "BITMAP - columnas"
+    label: "BITMAP - columnas",
   },
 
   habilitarPixelado: {
@@ -352,7 +373,6 @@ export const inputs = {
     default: 3.0,
     label: "PIXELADO - resolucion",
   },
-
 };
 
 export const presets = {
